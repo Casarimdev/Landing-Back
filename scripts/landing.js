36ef273e2bf7ff0edc379665e8be6c06838e1428
Casarimdev <casarim.dev@gmail.com>
2023-09-26 23:02:45 -0300

    bug-no sound

--- scripts/landing.js
+++ scripts/landing.js
@@ -22,6 +22,23 @@ sleep(15000).then(() => VettorD.style.display = 'inline')
 sleep(15000).then(() => aSale.href = "https://pay.hotmart.com/D86373027M?off=fn5bmfo6&checkoutMode=10&bid=1695232905378")
 
 
+
+// VIDEOS
+// VTURB-ESP
+let videoESP = document.getElementById("vid_6503757f909f89000a905de3")
+let thumbESP = document.getElementById("thumb_6503757f909f89000a905de3")
+let backdropESP = document.getElementById("backdrop_6503757f909f89000a905de3")
+videoESP.style.display = 'none'
+// thumbESP.style.display = 'none'
+// backdropESP.style.display = 'none'
+
+
+let videoUS = document.getElementById("video-US")
+let videoPT = document.getElementById("video-PT")
+
+videoPT.style.display = 'none'
+videoUS.style.display = 'none'
+
 // TEXTS
 
     // Container 1 
@@ -49,8 +66,16 @@ let TextoVenda4 = document.getElementById("TextVenda4")
 
 // SPANINH LANGUAGE
 function translateSpaninsh(){
+    // thumbESP.style.display = 'block'
+    // backdropESP.style.display = 'block'
+    videoESP.style.display = 'block'
+    videoESP.innerHTML= '<div id= "vid_6503757f909f89000a905de3" style="position:relative;width:900px;padding: 0.25% 0 0;height:auto;"><img id="thumb_6503757f909f89000a905de3" src="https://images.converteai.net/9714ed72-2eda-418b-bb68-df9ab62de4e3/players/6503757f909f89000a905de3/thumbnail.jpg" style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;display:block;" alt="img-adv"> <div id="backdrop_6503757f909f89000a905de3" style="position:absolute;top:0;width:100%;height:100%;-webkit-backdrop-filter:blur(5px);backdrop-filter:blur(5px);"> </div></div> <script type="text/javascript" id="scr_6503757f909f89000a905de3">var s = document.createElement("script"); s.src = "https://scripts.converteai.net/9714ed72-2eda-418b-bb68-df9ab62de4e3/players/6503757f909f89000a905de3/player.js", s.async = !0, document.head.appendChild(s);</script>'
+
+
+    videoUS.style.display = 'none'
+    videoPT.style.display = 'none'
     // Primeiro Container
-    Select.innerHTML = 'elige tu idioma'
+    Select.innerHTML = 'elige tu idioma: '
     H1Texto.innerHTML = '<span style="color: #ff0000;">El Método</span> que te devolverá tu espalda <br> <span style="color: #ff0000;">juvenil</span> EN <span style="color: #ff0000;">4 minutos</span>'
     Clique.innerHTML = 'HACA CLIC AQUI PARA COMPRAR'
     SaibaMais.innerHTML = '¿ES PARA VOS? APRENDER MEJOR'
@@ -70,8 +95,11 @@ function translateSpaninsh(){
 
 // ENGLISH LANGUAGE
 function translateEnglish(){
+    videoESP.style.display = 'none'
+    videoUS.style.display = 'block'
+    videoPT.style.display = 'none'
     // Primeiro Container
-    Select.innerHTML = 'select your language'
+    Select.innerHTML = 'select your language: '
     H1Texto.innerHTML = '<span style="color: #ff0000;">THE METHOD</span> that will turn your back <br> <span style="color: #ff0000;">YOUTH</span> in <span style="color: #ff0000;">4 MINUTES</span>'
     Clique.innerHTML = 'CLICK HERE TO BUY'
     SaibaMais.innerHTML = 'IT IS FOR YOU? LEARN BETTER'
@@ -91,8 +119,11 @@ function translateEnglish(){
 // PORTUGUÊS LANGUAGE
 
 function translatePortuguese(){
+    videoESP.style.display = 'none'
+    videoUS.style.display = 'none'
+    videoPT.style.display = 'block'
     // Primeiro Container
-    Select.innerHTML = 'Selecione seu idioma'
+    Select.innerHTML = 'Selecione seu idioma: '
     H1Texto.innerHTML = '<span style="color: #ff0000;">O método</span> que vai te dar de volta suas costas <br> <span style="color: #ff0000;">Novas</span> em <span style="color: #ff0000;">4 MINUTOS</span>'
     Clique.innerHTML = 'CLIQUE AQUI PARA COMPRAR'
     SaibaMais.innerHTML = 'É PARA TI? SAIBA MELHOR'
